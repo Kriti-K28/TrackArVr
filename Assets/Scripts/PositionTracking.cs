eb0383f71b34f372dfcb38fe09bb8664cfb4ab0b
Kriti Kumari <1905243@kiit.ac.in>
2022-05-24 01:30:19 +0530

    Added position script

---
+++ Assets/Scripts/PositionTracking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using UnityEngine.UI;
+public class PositionTracking : MonoBehaviour
+{
+    private Vector3 origPos;
+    public Text test;
+    void Start () {
+        origPos = gameObject.transform.position;
+    }
+
+    void Update () {
+
+        // if (Frame.TrackingState == FrameTrackingState.Tracking)
+        // {
+        var pose = Frame.Pose;
+        gameObject.transform.position = pose.position + origPos;
+        test.text=pose.position.ToString();
+        //}
+    }
+}
