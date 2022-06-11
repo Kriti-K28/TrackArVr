c251fc6f017c6cdf2d6feefe0eb76c6e5f999f18
Kriti Kumari <1905243@kiit.ac.in>
2022-06-11 22:42:22 +0530

    Pos Tracking and size optimization

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using UnityEngine.UI;
+public class FPS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PositionTracking.cs
+++ Assets/Scripts/PositionTracking.cs
@@ -7,7 +7,10 @@ public class PositionTracking : MonoBehaviour
 {
     private Vector3 origPos;
     public Text test;
+
+    private double prevTime = 0f,minVrFps=100f,maxVrFps=0f, minArFps=100f;
     void Start () {
+        prevTime = Frame.Timestamp;
         origPos = gameObject.transform.position;
     }
 
@@ -17,7 +20,23 @@ public class PositionTracking : MonoBehaviour
         // {
         var pose = Frame.Pose;
         gameObject.transform.position = pose.position + origPos;
-        test.text=pose.position.ToString();
+        
         //}
+        if( 1f/(Frame.Timestamp - prevTime)<minArFps)
+           minArFps= 1f/(Frame.Timestamp - prevTime);
+        if( (1f / Time.deltaTime)>maxVrFps)
+            maxVrFps= 1f / Time.deltaTime;
+        if( (1f / Time.deltaTime)<minVrFps)
+            minVrFps= 1f / Time.deltaTime;
+        
+         test.text = "AR FPS: " + System.Math.Round(1f/(Frame.Timestamp - prevTime),0) + "\n" + //approximate frames per second for AR tracking
+                    "VR FPS: " + 1f / Time.deltaTime+"\n"+
+                    "Pos:"+pose.position.ToString()+"\n"+
+                   // "min AR FPS: "+System.Math.Round(minArFps,0)+"\n"+
+                    "min AR FPS: "+System.Math.Round(minArFps,0)+"\n"+
+                    "min VR FPS: "+System.Math.Round(minVrFps,0)+"\n"+
+                    "max VR FPS: "+System.Math.Round(maxVrFps,0); //approximate frames per seconds for rendering
+		prevTime = Frame.Timestamp;
+        Debug.Log("Timestamp test: "+ Frame.Timestamp);
     }
 }
